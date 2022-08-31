5ceb904e0f40fd8d58e144a25a3745a24159c6fd
doanduc2000 <doanminhducvip2000@gmail.com>
2022-08-31 11:48:51 +0700

    update

--- Home/customer_dhh_1_0_0/js/customer_dhh_1_0_0.js
+++ Home/customer_dhh_1_0_0/js/customer_dhh_1_0_0.js
@@ -1,5 +1,6 @@
 const client = [{
-        img: "images/KH-1.jpg",
+        img: "Home/customer_dhh_1_0_0/images/KH-1.jpg",
+        videoId: "HFEzxRrfMTI",
         info: `     <p class="customer_dhh_1_0_0__name">
         KH.ĐOÀN PHƯƠNG UYÊN
     </p>
@@ -10,13 +11,14 @@ const client = [{
         NÂNG MŨI SIÊU CẤU TRÚC
     </p>
     <div class="customer_dhh_1_0_0__list">
-        <img src="images/KH-2-1.jpg" alt="">
-        <img src="images/KH-2-2.jpg" alt="">
+        <img width="246" height="305" src="Home/customer_dhh_1_0_0/images/KH-2-1.jpg" alt="">
+        <img width="246" height="305" src="Home/customer_dhh_1_0_0/images/KH-2-2.jpg" alt="">
     </div>`,
-        cate: "slider01",
+        cate: "slider1",
     },
     {
-        img: "images/KH-2.jpg",
+        img: "Home/customer_dhh_1_0_0/images/KH-2.jpg",
+        videoId: "HFEzxRrfMTI",
         info: `     <p class="customer_dhh_1_0_0__name">
         KH.ĐOÀN THÙY LINH
     </p>
@@ -27,13 +29,14 @@ const client = [{
         NÂNG MŨI CẤU TRÚC PUREFORM
     </p>
     <div class="customer_dhh_1_0_0__list">
-        <img src="images/KH-2-1.jpg" alt="">
-        <img src="images/KH-2-2.jpg" alt="">
+        <img width="246" height="305" src="Home/customer_dhh_1_0_0/images/KH-2-1.jpg" alt="">
+        <img width="246" height="305" src="Home/customer_dhh_1_0_0/images/KH-2-2.jpg" alt="">
     </div>`,
-        cate: "slider01",
+        cate: "slider1",
     },
     {
-        img: "images/KH-3.jpg",
+        img: "Home/customer_dhh_1_0_0/images/KH-3.jpg",
+        videoId: "HFEzxRrfMTI",
         info: `     <p class="customer_dhh_1_0_0__name">
         KH.THÚY VI
     </p>
@@ -44,13 +47,14 @@ const client = [{
         NÂNG NGỰC
     </p>
     <div class="customer_dhh_1_0_0__list">
-        <img src="images/KH-3-1.jpg" alt="">
-        <img src="images/KH-3-2.jpg" alt="">
+        <img width="246" height="305" src="Home/customer_dhh_1_0_0/images/KH-3-1.jpg" alt="">
+        <img width="246" height="305" src="Home/customer_dhh_1_0_0/images/KH-3-2.jpg" alt="">
     </div>`,
-        cate: "slider01",
+        cate: "slider1",
     },
     {
-        img: "images/KH4.jpg",
+        img: "Home/customer_dhh_1_0_0/images/KH4.jpg",
+        videoId: "HFEzxRrfMTI",
         info: `     <p class="customer_dhh_1_0_0__name">
         HÀNH TRÌNH VƯỢT 200KM MONG MỎI CÓ CON
     </p>
@@ -60,13 +64,14 @@ const client = [{
       <li><b>Kết quả:</b> Sinh đôi 1 trai 1 gái</li>
     </ul>
     <div class="customer_dhh_1_0_0__demo">
-    <img src="images/KH-4-1.jpg" alt="">
-    <img src="images/KH-4-2.jpg" alt="">
+    <img width="191" height="242" src="Home/customer_dhh_1_0_0/images/KH-4-1.jpg" alt="">
+    <img width="191" height="242" src="Home/customer_dhh_1_0_0/images/KH-4-2.jpg" alt="">
   </div>`,
         cate: "slider2",
     },
     {
-        img: "images/KH-5.jpg",
+        img: "Home/customer_dhh_1_0_0/images/KH-5.jpg",
+        videoId: "HFEzxRrfMTI",
         info: `     <p class="customer_dhh_1_0_0__name">
         "TRÁI NGỌT" SAU 15 NĂM "TÌM CON" TƯỞNG CHỪNG VÔ VỌNG
     </p>
@@ -76,13 +81,14 @@ const client = [{
     <li><b>Kết quả:</b> Sinh đôi 1 trai 1 gái</li>
 </ul>
     <div class="customer_dhh_1_0_0__demo">
-        <img src="images/KH-5-1.jpg" alt="">
-        <img src="images/KH-5-2.jpg" alt="">
+        <img width="191" height="242" src="Home/customer_dhh_1_0_0/images/KH-5-1.jpg" alt="">
+        <img width="191" height="242" src="Home/customer_dhh_1_0_0/images/KH-5-2.jpg" alt="">
     </div>`,
         cate: "slider2",
     },
     {
-        img: "images/KH-6.jpg",
+        img: "Home/customer_dhh_1_0_0/images/KH-6.jpg",
+        videoId: "HFEzxRrfMTI",
         info: `     <p class="customer_dhh_1_0_0__name">
         HẠNH PHÚC VỠ ÒA SAU 10 NĂM "KHÁT CON"
     </p>
@@ -92,13 +98,14 @@ const client = [{
     <li><b>Kết quả:</b> Sinh đôi 1 trai 1 gái</li>
 </ul>
     <div class="customer_dhh_1_0_0__demo">
-        <img src="images/KH-6-1.jpg" alt="">
-        <img src="images/KH-6-2.jpg" alt="">
+        <img width="191" height="242" src="Home/customer_dhh_1_0_0/images/KH-6-1.jpg" alt="">
+        <img width="191" height="242" src="Home/customer_dhh_1_0_0/images/KH-6-2.jpg" alt="">
     </div>`,
         cate: "slider2",
     },
     {
-        img: "images/KH-7.jpg",
+        img: "Home/customer_dhh_1_0_0/images/KH-7.jpg",
+        videoId: "piG_2v8kZzA",
         info: `     <p class="customer_dhh_1_0_0__name">
         ĐÓN CẶP SONG SINH SAU 2 THÁNG ĐIỀU TRỊ ĐA NANG BUỒNG TRỨNG
     </p>
@@ -108,13 +115,14 @@ const client = [{
     <li><b>Kết quả:</b> Sinh đôi 2 bé trai</li>
 </ul>
     <div class="customer_dhh_1_0_0__demo">
-        <img src="images/KH-7-1.jpg" alt="">
-        <img src="images/KH-7-2.jpg" alt="">
+        <img src="Home/customer_dhh_1_0_0/images/KH-7-1.jpg" alt="">
+        <img src="Home/customer_dhh_1_0_0/images/KH-7-2.jpg" alt="">
     </div>`,
         cate: "slider3",
     },
     {
-        img: "images/KH-8.jpg",
+        img: "Home/customer_dhh_1_0_0/images/KH-8.jpg",
+        videoId: "NIXi2K99gzQ",
         info: `     <p class="customer_dhh_1_0_0__name">
         HIẾM MUỘN 3 NĂM VÀ HẠNH PHÚC NGẬP TRÀN TRƯỚC THỀM NĂM MỚI
     </p>
@@ -124,13 +132,14 @@ const client = [{
     <li><b>Kết quả:</b> Sinh đôi </li>
 </ul>
     <div class="customer_dhh_1_0_0__demo">
-        <img src="images/KH-8-1.jpg" alt="">
-        <img src="images/KH-8-2.jpg" alt="">
+        <img width="191" height="242" src="Home/customer_dhh_1_0_0/images/KH-8-1.jpg" alt="">
+        <img width="191" height="242" src="Home/customer_dhh_1_0_0/images/KH-8-2.jpg" alt="">
     </div>`,
         cate: "slider3",
     },
     {
-        img: "images/KH-9.jpg",
+        img: "Home/customer_dhh_1_0_0/images/KH-9.jpg",
+        videoId: "wQpVoMBs2cw",
         info: `     <p class="customer_dhh_1_0_0__name">
         12 NĂM NGƯỢC XUÔI CHẠY CHỮA VÀ HẠNH PHÚC BẤT NGỜ CỦA BÀ MẸ 8X
     </p>
@@ -140,8 +149,8 @@ const client = [{
     <li><b>Kết quả:</b> Gia đình chị có thêm 1 bé gái</li>
     </ul>
     <div class="customer_dhh_1_0_0__demo">
-        <img src="images/KH-9-1.jpg" alt="">
-        <img src="images/KH-9-2.jpg" alt="">
+        <img width="191" height="242" src="Home/customer_dhh_1_0_0/images/KH-9-1.jpg" alt="">
+        <img width="191" height="242" src="Home/customer_dhh_1_0_0/images/KH-9-2.jpg" alt="">
     </div>`,
         cate: "slider3",
     },
@@ -150,45 +159,56 @@ const client = [{
 const customerItem = document.getElementsByClassName(
     "customer_dhh_1_0_0__item"
 );
-
 const customerClient = document.getElementsByClassName(
     "customer_dhh_1_0_0__client"
 )[0];
 
-const slider01 = [],
-    slider2 = [],
-    slider3 = [];
+const modalytb = document.getElementById("modalytb");
+
+const sliderFirst = [],
+    sliderSecond = [],
+    sliderThird = [];
 let autoPlay;
 
 //loc
 for (let i = 0; i < client.length; i++) {
-    if (client[i].cate == "slider01") {
-        slider01.push(client[i]);
+    if (client[i].cate == "slider1") {
+        sliderFirst.push(client[i]);
     } else if (client[i].cate == "slider2") {
-        slider2.push(client[i]);
+        sliderSecond.push(client[i]);
     } else if (client[i].cate == "slider3") {
-        slider3.push(client[i]);
+        sliderThird.push(client[i]);
     }
 }
 
 //button 1
-customerItem[0].addEventListener("click", addslider01);
+customerItem[0].addEventListener("click", addSlider1);
 
-function addslider01() {
+function addSlider1() {
     clearInterval(autoPlay);
-    addSlider(0, slider01);
+    addSlider(0, sliderFirst);
 }
 
 function addHTML(idx, data) {
     HTML2 = `
-            <div class="customer_dhh_1_0_0__left">
-            <img src="${data[idx].img}" alt="">
+            <div class="customer_dhh_1_0_0__left " id="openModal" data-video="${data[idx].videoId}">
+            <img width="616" height="425" src="${data[idx].img}" alt="">
+            <img width="92" height="92" class="customer_dhh_1_0_0__video" src="Home/customer_dhh_1_0_0/images/video.png" alt="">
             </div>
             <div class="customer_dhh_1_0_0__right">
                 ${data[idx].info}
             </div>`;
     customerClient.innerHTML = HTML2;
+    const openModal = document.getElementById("openModal");
+    openModal.addEventListener("click", function() {
+        document.querySelectorAll(
+            ".customer_dhh_1_0_0__ytbVideo iframe"
+        )[0].src = `//www.youtube-nocookie.com/embed/${data[idx].videoId}?rel=0&amp;controls=1&amp;autoplay=1&amp;nocookie=true&amp;mute=true`;
+        modalytb.style.display = "block";
+    });
 }
+
+
 
 function addSlider(number, data) {
     addHTML(number, data);
@@ -200,17 +220,17 @@ function addSlider(number, data) {
         }
         addHTML(index, data);
         index++;
-    }, 3000);
+    }, 8000);
 }
 
-addSlider(0, slider01);
+addSlider(0, sliderFirst);
 
 //button2
 customerItem[1].addEventListener("click", addSlider2);
 
 function addSlider2() {
     clearInterval(autoPlay);
-    addSlider(0, slider2);
+    addSlider(0, sliderSecond);
 }
 
 //button3
@@ -218,7 +238,7 @@ customerItem[2].addEventListener("click", addSlider3);
 
 function addSlider3() {
     clearInterval(autoPlay);
-    addSlider(0, slider3);
+    addSlider(0, sliderThird);
 }
 
 //active
@@ -235,4 +255,15 @@ function setActive(element, className, event) {
     }
 }
 
-setActive(".customer_dhh_1_0_0__item", "active", "click");
+
+
+//open, close modal
+const closeModal = document.getElementById("customer_dhh_1_0_0__ytbClose");
+const overlay = document.getElementById("customer_dhh_1_0_0__ytbBg");
+
+closeModal.addEventListener("click", function() {
+    modalytb.style.display = "none";
+});
+overlay.addEventListener("click", function() {
+    modalytb.style.display = "none";
+});
